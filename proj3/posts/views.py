5a2969b76b92612ef6806697f059edd932b02ffc
SanjayPJ <32980569+SanjayPJ@users.noreply.github.com>
2018-07-20 21:07:07 +0530

    first 10 completed

---
+++ proj3/posts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+def posts(request):
+     posts = Post.objects.all().order_by('date')
+     return render(request, 'posts/posts.html',{'posts' : posts})
